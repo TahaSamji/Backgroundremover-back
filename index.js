b587e0aa9e480b3803183d58bc1a15d1a9702b21
Right 97 <Right.97@rs.net>
2024-07-22 16:07:21 +0500

    vercel commit

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post("/", (req, res) => {
         //    await main();
         
         // let imgSource = 'tmp/picture.png';
-        const oImage = fs.readFileSync("https://backgroundremover-back.vercel.app/picture.png");
+        const oImage = fs.readFileSync("https://backgroundremover-back.vercel.app/var/task/picture.png");
         console.log("this is my image",oImage);
         console.log("this is my dir",process.cwd());
         console.log(path.join(process.cwd(),'/tmp/picture.png'));
