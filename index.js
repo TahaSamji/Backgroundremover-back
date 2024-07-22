86208067bc2b708ab09206ab9ad3faf83b3b08e0
Right 97 <Right.97@rs.net>
2024-07-22 12:42:58 +0500

    vercel commit

--- index.js
+++ index.js
@@ -69,11 +69,13 @@ app.post("/", (req, res) => {
         //    await main();
         
         // let imgSource = 'tmp/picture.png';
-        const oImage = fs.readFileSync('/tmp/picture.png');
+        const oImage = fs.readFileSync('tmp/picture.png');
         console.log("this is my image",oImage);
         console.log("this is my dir",process.cwd());
         console.log(path.join(process.cwd(),'/tmp/picture.png'));
-        let blob = await removeBackground('/gsg4/ffg');
+        let originalblob = new Blob([myimage.data], { type: 'image/png' });
+        let blob = await removeBackground(originalblob);
+     
      
         console.log("my blob",blob);
         const buffer = Buffer.from(await blob.arrayBuffer());
