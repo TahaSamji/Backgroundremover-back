7871f2d7fd318e7e01d6ab1183f73f03f979cf90
Right 97 <Right.97@rs.net>
2024-07-22 16:02:13 +0500

    vercel commit

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const process = require('process');
 const app = express();
 
 
-app.use(express.static('public'));
 
 
+app.use(express.static(path.join(process.cwd(), 'public')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
@@ -78,12 +78,12 @@ app.post("/", (req, res) => {
         //    await main();
         
         // let imgSource = 'tmp/picture.png';
-        const oImage = fs.readFileSync("https://backgroundremover-back.vercel.app/picture.png");
+        const oImage = fs.readFileSync("https://backgroundremover-back.vercel.app/var/task/picture.png");
         console.log("this is my image",oImage);
         console.log("this is my dir",process.cwd());
         console.log(path.join(process.cwd(),'/tmp/picture.png'));
         let originalblob = new Blob([myimage.data], { type: 'image/png' });
-        let blob = await removeBackground("https://backgroundremover-back.vercel.app/picture.png",config);
+        let blob = await removeBackground("https://backgroundremover-back.vercel.app/var/task/picture.png",config);
      
         console.log("my blob",blob);
         const buffer = Buffer.from(await blob.arrayBuffer());
